6b2f7e536d52b486eb98782875fb49e1ab74b32a
Sion Williams <sion5@hotmail.co.uk>
2020-03-19 11:02:22 +0000

    Enable integ tests

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,10 +3,9 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -mod=vendor -timeout=2m -parallel=4 -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
+
+go test ./... -mod=vendor -timeout=2m -parallel=4 -coverprofile=profile.out -covermode=atomic ${TESTARGS} $d
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    rm profile.out
+fi
